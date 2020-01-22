2a05e669ebf5c39647cbfb5b8b888be85458ae41
Ubuntu <ubuntu@ip-172-31-82-167.ec2.internal>
2020-01-22 17:56:37 +0000

    initial commit

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql 
 
 username = os.getenv('C9_USER')
@@ -10,12 +11,24 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
 try:
-    # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
+        list_of_names = ['fred', 'fred']
+        # prep string with same num of placeholders of in list
+        format_strings = ','.join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings), list_of_names) 
+        connection.commit()
+        
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
+    """
+    # Run a query
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+    # with connection.cursor() as cursor: # open connetion to create cursor + use connection to  create cursor 
+        sql = "SELECT * FROM Genre;"
         cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        for row in cursor: 
+            print(row)
+    """
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
